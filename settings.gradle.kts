c360f6d3e59c9d50df686c221ec2b7919e845cd5
Tellinq <59412384+Tellinq@users.noreply.github.com>
2025-07-16 14:50:38 -0400

    Update Kotlin JVM to 2.0.20

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,7 +23,7 @@ pluginManagement {
     }
 
     plugins {
-        kotlin("jvm") version("2.0.10")
+        kotlin("jvm") version("2.0.20")
         id("dev.deftu.gradle.multiversion-root") version("2.42.0")
     }
 }
